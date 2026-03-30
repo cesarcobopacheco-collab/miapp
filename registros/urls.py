cbc40a534931ac3679b7d82f5e99f89c8d0bad64
Cesar <cesarcobopacheco@gmail.com>
2026-03-29 19:21:00 -0500

    CRUD completo con DataTables

---
+++ registros/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lista, name='lista'),
+    path('agregar/', views.agregar, name='agregar'),
+    path('eliminar/<int:id>/', views.eliminar, name='eliminar'),
+    path('editar/<int:id>/', views.editar, name='editar'),
+]
--- registros/views.py
+++ registros/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Registro
 
-# Create your views here.
+def lista(request):
+    registros = Registro.objects.all()
+    return render(request, 'registros/lista.html', {'registros': registros})
+
+def agregar(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        descripcion = request.POST['descripcion']
+        Registro.objects.create(nombre=nombre, descripcion=descripcion)
+        return redirect('lista')
+    return render(request, 'registros/agregar.html')
+
+def eliminar(request, id):
+    registro = get_object_or_404(Registro, id=id)
+    registro.delete()
+    return redirect('lista')
+
+def editar(request, id):
+    registro = get_object_or_404(Registro, id=id)
+    if request.method == 'POST':
+        registro.nombre = request.POST['nombre']
+        registro.descripcion = request.POST['descripcion']
+        registro.save()
+        return redirect('lista')
+    return render(request, 'registros/editar.html', {'registro': registro})
