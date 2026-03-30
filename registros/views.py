498af90aad9e310855fd1a05db1d65e904e19e0e
Cesar <cesarcobopacheco@gmail.com>
2026-03-29 21:17:06 -0500

    login y autentificacion funcionando

--- registros/views.py
+++ registros/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Registro
 
+@login_required
 def lista(request):
     registros = Registro.objects.all()
     return render(request, 'registros/lista.html', {'registros': registros})
 
+@login_required
 def agregar(request):
     if request.method == 'POST':
         nombre = request.POST['nombre']
@@ -13,11 +16,13 @@ def agregar(request):
         return redirect('lista')
     return render(request, 'registros/agregar.html')
 
+@login_required
 def eliminar(request, id):
     registro = get_object_or_404(Registro, id=id)
     registro.delete()
     return redirect('lista')
 
+@login_required
 def editar(request, id):
     registro = get_object_or_404(Registro, id=id)
     if request.method == 'POST':
